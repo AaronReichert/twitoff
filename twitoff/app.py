8116b6952d84f83b03b792bb6f47977f457eaa97
AaronReichert <reichert.aaron.c@gmail.com>
2020-08-06 20:04:02 -0500

    working database inputs

---
+++ twitoff/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from .db_model import db, User, Tweet
+
+def create_app():
+    '''Create and configure an instance of the Flask application'''
+
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\Aaron\\Desktop\\twitoff\\twitoff\\twitoff_db.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    @app.route('/')
+    def root():
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    return app
