3aa7b9c8ab88ac4f093703af96c6bc7e92681718
AaronReichert <reichert.aaron.c@gmail.com>
2020-08-09 22:14:03 -0500

    module 2 done

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -8,19 +8,19 @@ class User(db.Model):
     id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     follower_count = db.Column(db.Integer, nullable=False)
-    # tweet ids are ordinal ints so we can fetch more recent tweets
-    # newest_tweet_id = db.Column(db.BigInteger, nullable=False)
+    # Tweet IDs are ordinal ints, so we can fetch most recent tweets
+    newest_tweet_id = db.Column(db.BigInteger, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-        
+
 class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     text = db.Column(db.Unicode(300))
     embedding = db.Column(db.PickleType, nullable=False)
     user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=('tweet'), lazy=True)
+    user = db.relationship('User', backref=db.backref('tweet', lazy=True))
 
     def __repr__(self):
-        return '<User %r>' % self.text
+        return '<Tweet %r>' % self.text
